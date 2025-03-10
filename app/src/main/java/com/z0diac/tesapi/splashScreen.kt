d06a936d701bd19c0a189e1c342903ba27143504
Kuahfi <mfauzandarmawan2010@gmail.com>
2025-03-10 22:45:45 +0700

    Commit kedua: Kahfi

---
+++ app/src/main/java/com/z0diac/tesapi/splashScreen.kt
@@ -0,0 +1,21 @@
+package com.z0diac.tesapi
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class splashScreen : AppCompatActivity() {
+
+    private val SPLASH_TIME: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+    }
+}
